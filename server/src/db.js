04d2c16e73079c0cee3a75b71103ac440791eb0f
Alan Acevedo <alanacevedo2001@hotmail.com>
2025-05-18 12:48:57 -0300

    Update db.js

--- server/src/db.js
+++ server/src/db.js
@@ -6,7 +6,8 @@ const {
   DATABASE_URL
 } = process.env;
 
-const sequelize = new Sequelize(DATABASE_URL, {
+const sequelize = new Sequelize("postgresql://postgres:TKIIceCchDxvCaCXKPvUZOeyhFmWYwzo@maglev.proxy.rlwy.net:18228/railway
+", {
   logging: false, // set to console.log to see the raw SQL queries
   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
 });
